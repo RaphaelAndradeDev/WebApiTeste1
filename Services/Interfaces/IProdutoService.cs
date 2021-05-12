2b3a2b783400b590bb18516697287eaa46d58d79
Rio Design <Rio_eDesign@hotmail.com>
2021-05-12 11:43:03 -0300

    Interface Concluída

---
+++ Services/Interfaces/IProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using Services.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IProdutoService : IDisposable
+    {
+        Task Adicionar(Produtos produtos);
+        Task Atualizar(Produtos produtos);
+        Task Remover(Guid id);
+    }
+}
---
+++ Services/Interfaces/IVendedorService.cs
@@ -0,0 +1,13 @@
+﻿using Services.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IVendedorService : IDisposable
+    {
+        Task Adicionar(Vendedor vendedor);
+        Task Atualizar(Vendedor vendedor);
+        Task Remover(Guid id);
+    }
+}
